c956b4d43f4155a8792748f7825612393e664d9d
Christopher <uraniumkid30@gmail.com>
2019-08-01 11:04:43 +0100

    added settings and updated company models

---
+++ PyBincom_v1/settings_readme.txt
@@ -0,0 +1,3 @@
+to run settings (python manage.py runserver wont work if settings.py is removed)
+
+python manage.py runserver --settings=PyBincom_v1.settings.filename
--- company/models.py
+++ company/models.py
@@ -1,9 +1,19 @@
 # models tut 1
 from django.db import models
+from django.urls import reverse
+# relatedname lists out all the children in a parent
+# related_query_name is used to filter in queries
+# null = True should never be used for text base field (None  and empty str)
+# blank is a form validation notion, form.is_valid()
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
 
 
-class Company(models.Model):
-    name = models.CharField(max_length=30)
+class Company(CommonInfo):
     about = models.TextField()
     email = models.EmailField()
     website = models.URLField()
@@ -11,21 +21,37 @@ class Company(models.Model):
     owner = models.CharField(max_length=150)
     vacancy = models.BooleanField()
     created_at = models.DateTimeField()
+    companies = models.Manager()
 
     def __str__(self):
         return self.name
 
-class Language(models.Model):
+    def get_absolute_url(self):
+        return reverse('company_details', kwargs={'pk':self.id})
+
+    class Meta:
+        verbose_name = 'company'
+        verbose_name_plural = 'companies'
+
+class Language(CommonInfo):
     LANG_TYPE = (('C', 'Compiled'),
                     ('I', 'Interpreted'),)
-    name = models.CharField(max_length=30)
     language_type = models.CharField(choices=LANG_TYPE, default='C', max_length=50)
+    languages = models.Manager()
+
     def __str__(self):
         return self.name
 
-class Framework(models.Model):
-    name = models.CharField(max_length=30)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE) #in sql language id is used
+    def get_absolute_url(self):
+        return reverse('language_details', kwargs={'pk': self.id})
+
+class Framework(CommonInfo):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE ,related_name='Frameworks',related_query_name='framework')
+    #in sql language id is used
+    frameworks = models.Manager()
+
+    def get_absolute_url(self):
+        return reverse('framework_details', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.name
@@ -38,8 +64,8 @@ class Programmer(models.Model):
 
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    company = models.ForeignKey(Company,on_delete=models.CASCADE)
-    language = models.ManyToManyField(Language,)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE,related_name='Programmers')
+    language = models.ManyToManyField(Language,related_name='Programmers',related_query_name='programmer')
     age = models.IntegerField()
     lang_level = models.CharField(choices=LANG_PROFICIENCY,default='B',max_length=50)
     employment_level = models.CharField(choices=EMPLOYMENT_LEVEL, default='J',max_length=50)
@@ -48,6 +74,7 @@ class Programmer(models.Model):
     salary_paid = models.BooleanField()
     marital_status = models.BooleanField()
     date_employed = models.DateTimeField()
+    programmers = models.Manager()
 
     @property
     def total_earnings(self):
@@ -59,6 +86,10 @@ class Programmer(models.Model):
 
 
     def __str__(self):
-        return self.first_name + ' ' +self.last_name
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('programmer_details', kwargs={'pk': self.id})
+
 
 
